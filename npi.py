169ece34f798a056cc70992a70b5c34b54156b1b
Dellau Nicolas <37139037+Dellaunicolas@users.noreply.github.com>
2018-05-07 19:20:54 +0200

    Dévellopement de la calculatrice

--- npi.py
+++ npi.py
@@ -1,8 +1,20 @@
 #!/usr/bin/env python3
 
-# Le même programme avec la gestion des erreurs
+# Le programme avec la gestion des erreurs
 import operator
-
+from tkinter import *
+  
+root = Tk()
+x0 = "0"
+x1 = "1"
+x2 = "2"
+x3 = "3"
+x4 = "4"
+x5 = "5"
+x6 = "6"
+x7 = "7"
+x8 = "8"
+x9 = "9"
 operateurs = {"+": operator.add,
              "-": operator.sub,
              "*": operator.mul,
@@ -24,8 +36,8 @@ def _eval(expr):
     for w in expr:
         if w in operateurs.keys():
             try:
-                op1 = result.pop()
-                op2 = result.pop()
+                op1 = resultat.pop()
+                op2 = resultat.pop()
             except IndexError:
                 print("Erreur : Trop peut d'opérande pour l'opération "+w)
                 erreur = True
@@ -50,3 +62,40 @@ def _print(resultat):
 if __name__ == "__main__":
     while True:
         _print(_eval(_read()))
+
+
+  
+# ==== Functions ====
+  #-----Prog Principal-----#
+
+fen = Tk.Tk()
+fen.title('Calculatrice')
+
+fra1 = Frame(fen)
+fra1.grid(row=1,column=0)
+Button(fra1, text = '9', command= x9).grid(row=2, column = 2, padx = 3, pady = 3)
+Button(fra1, text = '8', command= x8).grid(row=2, column = 1, padx = 3, pady = 3)
+Button(fra1, text = '7', command= x7).grid(row=2, column = 0, padx = 3, pady = 3)
+Button(fra1, text = '6', command= x6).grid(row=3, column = 2, padx = 3, pady = 3)
+Button(fra1, text = '5', command= x5).grid(row=3, column = 1, padx = 3, pady = 3)
+Button(fra1, text = '4', command= x4).grid(row=3, column = 0, padx = 3, pady = 3)
+Button(fra1, text = '3', command= x3).grid(row=4, column = 2, padx = 3, pady = 3)
+Button(fra1, text = '2', command= x2).grid(row=4, column = 1, padx = 3, pady = 3)
+Button(fra1, text = '1', command= x1).grid(row=4, column = 0, padx = 3, pady = 3)
+Button(fra1, text = '0', command= x0).grid(row=5, column = 2, padx = 3, pady = 3)
+
+z = StringVar()
+entree=Entry(fen,textvariable=z)
+entree.grid(row=0,column=0)
+z.set("0.")
+
+Button(fra1, text= '+', command = aplus).grid(row=2,column=5, padx = 3, pady = 3)
+Button(fra1, text= '-', command = amoins).grid(row=3,column=5, padx = 3, pady = 3)
+Button(fra1, text= '*', command = afois).grid(row=2,column=6, padx = 3, pady = 3)
+Button(fra1, text= '/', command = adiv).grid(row=3,column=6, padx = 3, pady = 3)
+Button(fra1, text= '.', command = point).grid(row=4,column=5, padx = 3, pady = 3)
+Button(fra1, text= '=', command = aegal).grid(row=4,column=6, padx = 3, pady = 3)
+Button(fra1, text= 'C', command = clear).grid(row=5, column=6, padx = 3, pady = 3)
+Button(fra1, text= 'Pile', command = pile).grid(row=7, column=7, padx = 3, pady = 3)
+
+Button(fen,text='Quitter',command = fen.destroy).grid(row=6,column=7)
